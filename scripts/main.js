09956e5fe0e1854099a86717092d10f8e9841ea7
Ardi <ardi.winardi91@gmail.com>
2017-07-04 13:50:27 +0700

    change and modify

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,3 @@
-/*
-*
-*  Push Notifications codelab
-*  Copyright 2015 Google Inc. All rights reserved.
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*      https://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License
-*
-*/
-
-/* eslint-env browser, es6 */
-
 'use strict';
 
 const applicationServerPublicKey = 'BDYmWnemb0QcQKXCPJvZmnfkCHKYyJvPSbTm1mmTR6lwOk-92uTswvEzSKsr0Rvnn35Tpyz1eExFe_285mheu5Y';
@@ -61,7 +40,7 @@ function initialiseUI() {
 	subscribeUser();
   
 	// Set the initial subscription value
-	swRegistration.pushManager.getSubscription();
+	swRegistration.pushManager.getSubscription()
 	.then(function(subscription) {
 		isSubscribed = !(subscription === null);
 
