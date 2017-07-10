d318f44185311b4645f48c093d17061ac2fb2178
Ardi <ardi.winardi91@gmail.com>
2017-07-10 09:29:14 +0700

    ok

--- sw.js
+++ sw.js
@@ -1,25 +1,3 @@
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
 self.addEventListener('push', function(event) {
 	
 	if (!(self.Notification && self.Notification.permission === 'granted')) {
@@ -31,18 +9,12 @@ self.addEventListener('push', function(event) {
         var res = JSON.parse(str);
 		title = res.title;
 		options = res.options;
-		
-		setCookie('title',title,30);
-		setCookie('options',JSON.stringify(options),30);
-		
     } catch (e) {
-		
 		console.log(e);
-		//var title = getCookie('title');
-		//var options = JSON.parse(getCookie('options'));
 	}
     
 	const notificationPromise = self.registration.showNotification(title, options);
+	alert(123);
 	event.waitUntil(notificationPromise);
 });
 
