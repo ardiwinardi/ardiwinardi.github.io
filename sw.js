d887ac8e55c08ad3c70cab641fbf69a3464a0054
Ardi <ardi.winardi91@gmail.com>
2017-07-10 09:10:18 +0700

    commit1

--- sw.js
+++ sw.js
@@ -1,3 +1,25 @@
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
 self.addEventListener('push', function(event) {
 	
 	if (!(self.Notification && self.Notification.permission === 'granted')) {
@@ -5,17 +27,17 @@ self.addEventListener('push', function(event) {
 	}
 	
 	var str = event.data.text();
-	
-	var title = 'Adzan Reminder';
-	var options = {
-		body: 'Test'
-	}
-	
 	try {
         var res = JSON.parse(str);
 		title = res.title;
 		options = res.options;
-    } catch (e) {}
+		
+		setCookie('title',title,360);
+		setCookie('options',JSON.stringify(options),360);
+    } catch (e) {
+		var title = getCookie('title');
+		var options = JSON.parse(getCookie('options'));
+	}
     
 	const notificationPromise = self.registration.showNotification(title, options);
 	event.waitUntil(notificationPromise);
